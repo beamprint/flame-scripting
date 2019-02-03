de4dc063d11a3abb6d0c112316267f956ab85605
Dieter Brehm <inkering@gmail.com>
2019-02-03 02:45:11 -0500

    implement web sockets server to send to esp

--- flame-detector/flame-detect.py
+++ flame-detector/flame-detect.py
@@ -5,6 +5,13 @@ Flame detector - measures how fire-y an image is
 
 import sys
 import cv2
+import asyncio
+import os.path
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+from datetime import timedelta
 
 
 def callback(x):
@@ -12,14 +19,6 @@ def callback(x):
     Callback necessary for the createTrackbar call
     """
 
-
-cv2.namedWindow('slider control window')
-cv2.createTrackbar("h range low", "slider control window", 0, 360, callback)
-cv2.createTrackbar("s range low", "slider control window", 30, 255, callback)
-cv2.createTrackbar("v range low", "slider control window", 0, 255, callback)
-cv2.createTrackbar("h range high", "slider control window", 0, 360, callback)
-cv2.createTrackbar("s range high", "slider control window", 30, 255, callback)
-cv2.createTrackbar("v range high", "slider control window", 0, 255, callback)
 
 def detect_flame(image, visualize=True):
     """
@@ -32,16 +31,16 @@ def detect_flame(image, visualize=True):
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
     # receive values from track bars and apply them to the lower limit of the HSV range
-    hmin = cv2.getTrackbarPos("h range low", "slider control window")
-    smin = cv2.getTrackbarPos("s range low", "slider control window")
-    vmin = cv2.getTrackbarPos("v range low", "slider control window")
-    hhigh = cv2.getTrackbarPos("h range high", "slider control window")
-    shigh = cv2.getTrackbarPos("s range high", "slider control window")
-    vhigh = cv2.getTrackbarPos("v range high", "slider control window")
-    # lower ideal for red = (0,135,052)
-    lower = (hmin, smin, vmin)
-    #upper ideal for red = = (222, 255, 255)
-    upper = (hhigh, shigh, vhigh)
+    #hmin = cv2.getTrackbarPos("h range low", "slider control window")
+    #smin = cv2.getTrackbarPos("s range low", "slider control window")
+    #vmin = cv2.getTrackbarPos("v range low", "slider control window")
+    #hhigh = cv2.getTrackbarPos("h range high", "slider control window")
+    #shigh = cv2.getTrackbarPos("s range high", "slider control window")
+    #vhigh = cv2.getTrackbarPos("v range high", "slider control window")
+    lower  = (0,135,52)
+    #lower = (hmin, smin, vmin)
+    upper = (222, 255, 255)
+    #upper = (hhigh, shigh, vhigh)
 
     # create a mask filtered by the bounds
     mask = cv2.inRange(hsv, lower, upper)
@@ -53,11 +52,14 @@ def detect_flame(image, visualize=True):
     ret, thresh1 = cv2.threshold(mask, 150, 255, cv2.THRESH_BINARY)
 
     # find contours and draw them
-    contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh1,
+                                           cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
         cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
         c = max(contours, key=cv2.contourArea)
+        largest_area = cv2.contourArea(c)
         x, y, w, h = cv2.boundingRect(c)
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 5)
 
@@ -66,39 +68,101 @@ def detect_flame(image, visualize=True):
         cv2.imshow('mask', mask)
         cv2.imshow('mask', thresh1)
 
+    if 'largest_area' in locals():
+        return largest_area
+    return 0
 
-# Test code for processing data directly from the computer webcam or a
-# saved image file
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-        if filename == 'webcam':
-            # Process the webcam in realtime
-            # "0" here means "the first webcam on your system"
-            camera = cv2.VideoCapture(0)
 
-            while True:
-                # Get the latest image from the webcam
-                ok, image = camera.read()
-                if not ok:
-                    print("Unable to open webcam...")
-                    break
+### Test code for processing data directly from the computer webcam or a saved image file
+# if __name__ == '__main__':
+#     if len(sys.argv) == 2:
+#         filename = sys.argv[1]
+#         if filename == 'webcam':
+#             # Process the webcam in realtime
+#             camera = cv2.VideoCapture(0)  # "0" here means "the first webcam on your system"
 
-                # Process it
-                coordinates = detect_flame(image)
-                print(coordinates)
+#             while True:
+#                 # Get the latest image from the webcam
+#                 ok, image = camera.read()
+#                 if not ok:
+#                     print("Unable to open webcam...")
+#                     break
 
-                # Exit if the "Esc" key is pressed
-                key = cv2.waitKey(1)
-                if key == 27:
-                    break
-        else:
-            # Read data from an image file
-            image = cv2.imread(filename)
-            # coordinates = detect_flame(image)
-            # print(coordinates)
-            cv2.waitKey(0)
+#                 # Process it
+#                 coordinates = detect_helmet_coordinates(image)
+#                 print(coordinates)
 
-    else:
-        print('Usage: When you run this script, provide either the filename of an image or the string "webcam"')
-#
+#                 # Exit if the "Esc" key is pressed
+#                 key = cv2.waitKey(1)
+#                 if key == 27:
+#                     break
+#         else:
+#             # Read data from an image file
+#             image = cv2.imread(filename)
+#             coordinates = detect_helmet_coordinates(image)
+#             print(coordinates)
+#             cv2.waitKey(0)
+
+#     else:
+#         print('Usage: When you run this script, provide either the filename of an image or the string "webcam"')
+
+cap = cv2.VideoCapture(0)
+success, image = cap.read()
+flame = detect_flame(image, visualize=True)
+clients = []
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        print("origin: " + origin)
+        return True
+
+    # the client connected
+    def open(self):
+        print("New client connected")
+        self.write_message("You are connected")
+        clients.append(self)
+
+    # the client sent the message
+    def on_message(self, message):
+        print("message: " + message)
+        self.write_message(message)
+
+    # client disconnected
+    def on_close(self):
+        print("Client disconnected")
+        clients.remove(self)
+
+        
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    left = leftMax - leftMin
+    right = rightMax - rightMin
+    scaled = float(value - leftMin) / float(left)
+    return rightMin + (scaled * right)
+
+
+def send_message_to_clients():
+    old_val = 0
+    alpha = 0.6
+    try:
+        success, image = cap.read()
+        flame = detect_flame(image, visualize=True)
+        val = int(100 * (0.3 * flame + (1 - alpha) * old_val)) / 100
+        old_val = val
+        val = translate(val, 0, 40000, 0, 1)
+        for client in clients:
+            print("sending a message")
+            if val > 0.3:
+                client.write_message("1")
+            else:
+                client.write_message("0")
+    finally:
+        tornado.ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1),
+                                                     send_message_to_clients)
+
+
+socket = tornado.web.Application([(r"/", WebSocketHandler), ])
+if __name__ == "__main__":
+    socket.listen(8888)
+    tornado.ioloop.IOLoop.instance().add_timeout(timedelta(seconds=0.05),
+                                                 send_message_to_clients)
+    tornado.ioloop.IOLoop.instance().start()
